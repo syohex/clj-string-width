db413bfd1221e977ddaaed53ae8f6c62c31e6f9d
Syohei YOSHIDA <syohex@gmail.com>
2015-03-30 21:49:23 +0900

    Initial implement

--- test/string_width/core_test.clj
+++ test/string_width/core_test.clj
@@ -2,6 +2,18 @@
   (:require [clojure.test :refer :all]
             [string-width.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest character-width-test
+  (testing "width of character"
+    (is (= (character-width \世) 2))
+    (is (= (character-width \界) 2))
+    (is (= (character-width \ｾ) 1))
+    (is (= (character-width \ｶ) 1))
+    (is (= (character-width \ｲ) 1))
+    (is (= (character-width \★) 2))
+    (is (= (character-width \u0000) 0))
+    (is (= (character-width \u0001) 1))))
+
+(deftest string-width-test
+  (testing "Total width of string"
+    (is (= (string-width "■㈱の世界①") 12))
+    (is (= (string-width "スター☆") 8))))
