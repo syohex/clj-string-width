c397763c331df97d22f53c170a54facadfb7f99d
Syohei YOSHIDA <syohex@gmail.com>
2015-03-30 21:49:31 +0900

    Update project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject string-width "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject string-width "0.0.1"
+  :description "String width library"
+  :url "http://github.com/syohex/clj-string-width"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
